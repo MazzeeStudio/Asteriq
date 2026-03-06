dd36ca907c76390c72b22ea1ad1fa9cea23fbcb3
Nero Veloce <neroveloce@icloud.com>
2026-03-06 12:39:24 +0100

    Fix DeviceMatchingService to take IHidHideService instead of concrete type

--- src/Asteriq/Services/DeviceMatchingService.cs
+++ src/Asteriq/Services/DeviceMatchingService.cs
@@ -1,4 +1,5 @@
 using Asteriq.Models;
+using Asteriq.Services.Abstractions;
 
 namespace Asteriq.Services;
 
@@ -7,9 +8,9 @@ namespace Asteriq.Services;
 /// </summary>
 public class DeviceMatchingService
 {
-    private readonly HidHideService _hidHide;
+    private readonly IHidHideService _hidHide;
 
-    public DeviceMatchingService(HidHideService hidHide)
+    public DeviceMatchingService(IHidHideService hidHide)
     {
         _hidHide = hidHide;
     }
